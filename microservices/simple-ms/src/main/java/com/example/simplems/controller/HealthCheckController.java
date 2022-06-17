531b4443681b3d521a7e9794be29ca552dfbeea5
Alessandro Dionisi <a.dionisi@entando.com>
2022-06-17 16:33:40 +0200

    First version

---
+++ microservices/simple-ms/src/main/java/com/example/simplems/controller/HealthCheckController.java
@@ -0,0 +1,11 @@
+package com.example.simplems.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public void healthCheck() {}
+}
