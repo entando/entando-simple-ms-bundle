48f8923c351e7904c6023be626506532d74b2398
Alessandro Dionisi <a.dionisi@entando.com>
2022-06-23 11:47:17 +0200

    Enabled CORS

--- microservices/simple-ms/src/main/java/com/example/simplems/controller/DemoApiController.java
+++ microservices/simple-ms/src/main/java/com/example/simplems/controller/DemoApiController.java
@@ -2,12 +2,14 @@ package com.example.simplems.controller;
 
 import java.util.Date;
 import java.util.Map;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api")
 public class DemoApiController {
 
